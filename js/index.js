4355c3d96d4ad5bc93df0b4606f102fb55e01481
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-07-24 11:59:49 -0300

    audio e setTimeout

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
+//variável que armazena o audio que será executado na função
+let audio = document.getElementById('somMotor');
 //Objeto com propriedades e métodos
 const carro = {
     marca: 'ford', modelo: 'mustang', ano: '1974', placa: 'US-JSON', 
     buzina: function() {alert('fon fon')}, 
-    roncoMotor: function(){alert('Vruuuuuuuummmm')}
+    roncoMotor: async function(){
+        audio.play();
+        //segunda ação com atraso de 3 segundos
+        setTimeout(function(){
+            alert('Vruuuuuuuummmm');}, 3000);
+    }
 };
-//dados a serem exibidos no identificados "texto" do index.html.
+
+//dados a serem exibidos no identificador "texto" do index.html.
 document.getElementById('texto').innerHTML='Este é meu novo carro. É um '+carro.marca+
 ' '+carro.modelo+' '+carro.ano+'!';
 
